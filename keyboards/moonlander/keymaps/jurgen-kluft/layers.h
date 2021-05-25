9ac01c4ac76666274ccc2cdb298336cd2008302e
Jurgen <jurgen.kluft@gmail.com>
2021-05-25 09:04:03 +0800

    * moonlander config

---
+++ keyboards/moonlander/keymaps/jurgen-kluft/layers.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+enum layers {
+    _QWERTY_MOD = 0,
+    _QWERTY,
+    _RSTHD_MOD,
+    _RSTHD,
+    _QWERTY_CAPS,
+    _RSTHD_CAPS,
+    _SMART_NUM,
+    _SYM,
+    _NUM,
+    _NAV,
+    _FNC,
+    _MOUS,
+    _GAMEL,
+    _GAMER,
+    _HR_MOD_L,
+    _HR_MOD_R
+};
