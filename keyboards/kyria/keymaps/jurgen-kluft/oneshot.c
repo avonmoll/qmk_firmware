3e5ceb9d1b3cdbd9df02d674066cc02d0f15096e
Jurgen <jurgen.kluft@gmail.com>
2021-06-05 15:41:31 +0800

    * oneshot.c; small code cleanup

--- keyboards/kyria/keymaps/jurgen-kluft/oneshot.c
+++ keyboards/kyria/keymaps/jurgen-kluft/oneshot.c
@@ -15,9 +15,11 @@ void toggle_oneshot_hold(oneshot_state* state)
 }
 #endif
 
-
 void update_oneshot(oneshot_state* state, uint16_t mod, uint16_t trigger, uint16_t keycode, keyrecord_t* record)
 {
+    // @TODO: Can still extract MOD_BIT(mod) out to the caller site
+    uint16_t modbit = MOD_BIT(mod);
+    
     if (keycode == trigger)
     {
         if (record->event.pressed)
@@ -25,7 +27,7 @@ void update_oneshot(oneshot_state* state, uint16_t mod, uint16_t trigger, uint16
             // Trigger keydown
             if ((*state & os_state_mask) == os_up_unqueued)
             {
-                add_mods(MOD_BIT(mod));
+                add_mods(modbit);
             }
             *state = *state | os_down_unused;
         }
@@ -37,8 +39,8 @@ void update_oneshot(oneshot_state* state, uint16_t mod, uint16_t trigger, uint16
                 case os_down_unused:
                     // If we didn't use the mod while trigger was held, queue it.
                     *state = (*state & ~os_state_mask) | os_up_queued;
-                    del_mods(MOD_BIT(mod));
-                    add_oneshot_mods(MOD_BIT(mod));
+                    del_mods(modbit);
+                    add_oneshot_mods(modbit);
                     break;
                 case os_down_unused | os_up_queued:
                     *state = (*state ^ os_down_unused);
@@ -46,20 +48,20 @@ void update_oneshot(oneshot_state* state, uint16_t mod, uint16_t trigger, uint16
                     toggle_oneshot_hold(state);
                     if (is_oneshot_hold(*state))
                     {
-                        del_oneshot_mods(MOD_BIT(mod));
-                        add_mods(MOD_BIT(mod));
+                        del_oneshot_mods(modbit);
+                        add_mods(modbit);
                     }
                     else
                     {
-                        del_mods(MOD_BIT(mod));
-                        add_oneshot_mods(MOD_BIT(mod));
+                        del_mods(modbit);
+                        add_oneshot_mods(modbit);
                     }
 #endif
                     break;
                 case os_down_used:
                     // If we did use the mod while trigger was held, unregister it.
                     *state = os_up_unqueued;
-                    del_mods(MOD_BIT(mod));
+                    del_mods(modbit);
                     break;
                 default: 
                     break;
@@ -75,8 +77,8 @@ void update_oneshot(oneshot_state* state, uint16_t mod, uint16_t trigger, uint16
                 if ((*state & os_state_mask) != os_up_unqueued)
                 {
                     // Turn off mod
-                    del_mods(MOD_BIT(mod));
-                    del_oneshot_mods(MOD_BIT(mod));
+                    del_mods(modbit);
+                    del_oneshot_mods(modbit);
                 } 
                 *state = os_up_unqueued;
             } 
